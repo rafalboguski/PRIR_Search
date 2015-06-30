3f8fa48867a78c5564f5db44a42c849dd5ec90e0
rafalboguski <boguskirafal@gmail.com>
2015-06-30 20:01:06 +0200

    fixed scaling for more than 32 threads

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -7,6 +7,18 @@ import java.util.concurrent.*;
 
 public class Controller {
 
+    private ArrayList<Book>[] books;
+    private ArrayDeque<Result> results;
+
+    ExecutorService executor;
+    List<Future<ArrayDeque<Result>>> list;
+
+    private int adding_quee_id = 0;
+
+    // number of jobs for threads
+    private int DATA_DIVIDER = Runtime.getRuntime().availableProcessors() * 10;
+
+
     public Controller() {
 
         books = new ArrayList[DATA_DIVIDER];
@@ -15,56 +27,43 @@ public class Controller {
         for (int i = 0; i < DATA_DIVIDER; i++)
             books[i] = new ArrayList<>();
 
-        executor = Executors.newCachedThreadPool(); // tutaj mozna sie bawic roznymi rodzajami executorow casched imo najlepszy
+        executor = Executors.newCachedThreadPool();
     }
 
 
-    private int DATA_DIVIDER = 44; // Liczba ksiazek/DATA_DIVIDER = ksiazka w jednym zadaniu do pool-a
-    private int THREADS_NUM = 4;  // tylko do testow innych executorow newCachedThreadPool daje zawsze max ilosc watkow
-
-    private ArrayList<Book>[] books;
-    private ArrayDeque<Result> results;
-
-    private int adding_quee_id = 0;
-
     synchronized public void addBook(String filename, String data, String folder) {
-        books[adding_quee_id++].add(new Book(filename,data,folder));
-        if(adding_quee_id>= DATA_DIVIDER)
-            adding_quee_id=0;
+        books[adding_quee_id++].add(new Book(filename, data, folder));
+        if (adding_quee_id >= DATA_DIVIDER)
+            adding_quee_id = 0;
     }
 
     synchronized public LinkedList<Book.Data> getBooks() {
         LinkedList<Book.Data> result = new LinkedList<>();
         for (int i = 0; i < DATA_DIVIDER; i++) {
-            for(Book b:books[i]){
+            for (Book b : books[i]) {
                 result.add(b.getData());
             }
         }
         return result;
     }
 
-    ExecutorService executor;
-    List<Future<ArrayDeque<Result>>> list;
+
     synchronized public ArrayDeque<Result> search(String word) throws InterruptedException {
 
         results = new ArrayDeque<Result>();
-
         list = new ArrayList<Future<ArrayDeque<Result>>>();
-        long join = System.currentTimeMillis();
 
-        for(int i=0; i< DATA_DIVIDER; i++){
-            list.add( executor.submit(new PSearchBook(books[i], word)));
+        // parallel begin
+
+        for (int i = 0; i < DATA_DIVIDER; i++) {
+            list.add(executor.submit(new PSearchBook(books[i], word)));
         }
 
         for(Future<ArrayDeque<Result>> fut : list)
-            try { results.addAll(fut.get()); } catch (InterruptedException | ExecutionException e) {}
+            try { results.addAll(fut.get()); } catch (InterruptedException | ExecutionException e) {System.out.println(e);}
 
-
-        //System.out.println((System.currentTimeMillis() - join));
+        // parallel end
 
         return results;
     }
-
-
-
 }
