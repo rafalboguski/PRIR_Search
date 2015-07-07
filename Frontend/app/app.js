09915a50f0da9c82140bcbf1f7da23b4e768dcc8
rafalboguski <boguskirafal@gmail.com>
2015-07-08 00:00:29 +0200

    Removing unnecessary files and dependencies in frontend module

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -1,11 +1,10 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
+
 angular.module('myApp', [
   'ngRoute',
   'myApp.results',
-  'myApp.books',
-  'myApp.version'
+  'myApp.books'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/books'});
