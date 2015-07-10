cb73a9f32aba3683f9e9bf8e829069f91ecb5870
rafalboguski <boguskirafal@gmail.com>
2015-07-10 15:51:27 +0200

    Breadcrumbs

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -2,31 +2,27 @@
 
 
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.results',
-  'myApp.books',
-  'myApp.utils',
-  'ngAnimate',
-  'ui.bootstrap'
+    'ngRoute',
+    'myApp.results',
+    'myApp.books',
+    'myApp.utils',
+    'ngAnimate',
+    'ui.bootstrap'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/books'});
-}])
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/books'});
+    }])
 
 
+    .service('apiService', function ($http) {
 
+        var apiUrl = 'http://localhost:4567';
 
-.service('GlobalService', function($http) {
-    var apiUrl = 'http://localhost:4567';
+        this.Search = function (word) {
+            return $http.get(apiUrl + '/search/' + word);
+        };
+        this.getFiles = function () {
+            return $http.get(apiUrl + '/files' );
+        };
+    });
 
-
-    this.Search = function(word) {
-        return $http.get(apiUrl + '/search/' + word);
-    };
-
-})
-
-.run(function($rootScope, GlobalService) {
-        return $rootScope.GlobalService = GlobalService;
-    }
-);
