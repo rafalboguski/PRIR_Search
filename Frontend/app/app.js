91e66ebf01942a7b8b267f95507462181a60ca7a
rafalboguski <boguskirafal@gmail.com>
2015-07-22 13:16:58 +0200

    Large data performance fix, ng-repeat limit

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -14,21 +14,25 @@ angular.module('myApp', [
     }])
 
 
-    .factory('myService', function($http) {
-        var myService = {
-            async: function() {
-                // $http returns a promise, which has a then function, which also returns a promise
-                var promise = $http.get('http://localhost:4567' + '/search/' + 'a').then(function (response) {
-                    // The then function here is an opportunity to modify the response
-                    console.log(response);
-                    // The return value gets picked up by the then in the controller.
-                    return response;
-                });
-                // Return the promise to the controller
-                return promise;
+    .service('myService', function ($http) {
+        var myData = null;
+
+        var promise = $http.get('http://localhost:4567' + '/search/' + 'a').success(function (data) {
+            myData = data;
+        });
+
+        return {
+            promise: promise,
+            setData: function (data) {
+                console.log('SetData: ');
+                myData = data;
+
+            },
+            doStuff: function () {
+                console.log('DoStuff: ',myData);
+                return myData;//.getSomeData();
             }
         };
-        return myService;
     })
 
     .service('apiService', function ($http) {
@@ -37,9 +41,20 @@ angular.module('myApp', [
 
         this.Search = function (word) {
             return $http.get(apiUrl + '/search/' + word);
+
+            //return $q(function(resolve, reject) {
+            //    setTimeout(function() {
+            //        if (word) {
+            //            resolve('Hello, ' + word + '!');
+            //        } else {
+            //            reject('Greeting ' + word + ' is not allowed.');
+            //        }
+            //    }, 5000);
+            //});
         };
         this.getFiles = function () {
             return $http.get(apiUrl + '/files');
+
         };
 
         this.addFile = function (title, content, folder) {
@@ -55,27 +70,26 @@ angular.module('myApp', [
             });
         };
 
-
     })
 
-.filter('makeRange', function() {
-    return function(input) {
-        var lowBound, highBound;
-        switch (input.length) {
-            case 1:
-                lowBound = 0;
-                highBound = parseInt(input[0]) - 1;
-                break;
-            case 2:
-                lowBound = parseInt(input[0]);
-                highBound = parseInt(input[1]);
-                break;
-            default:
-                return input;
-        }
-        var result = [];
-        for (var i = lowBound; i <= highBound; i++)
-            result.push(i);
-        return result;
-    };
-});
+    .filter('makeRange', function () {
+        return function (input) {
+            var lowBound, highBound;
+            switch (input.length) {
+                case 1:
+                    lowBound = 0;
+                    highBound = parseInt(input[0]) - 1;
+                    break;
+                case 2:
+                    lowBound = parseInt(input[0]);
+                    highBound = parseInt(input[1]);
+                    break;
+                default:
+                    return input;
+            }
+            var result = [];
+            for (var i = lowBound; i <= highBound; i++)
+                result.push(i);
+            return result;
+        };
+    });
--- Frontend/app/results/results.js
+++ Frontend/app/results/results.js
@@ -5,7 +5,12 @@ angular.module('myApp.results', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/results/:word', {
             templateUrl: 'results/results.html',
-            controller: 'ResultsCtrl'
+            controller: 'ResultsCtrl',
+            resolve: {
+                'MyServiceData': function (myService) {
+                    return myService.promise;
+                }
+            }
         });
     }])
 
@@ -16,22 +21,39 @@ angular.module('myApp.results', ['ngRoute'])
             //$scope.hour = 13;
             $scope.word = $routeParams.word;
 
-            myService.async().then(function(d) {
-                $scope.searchTime = d.data.searchTime;
-                $scope.files = d.data.data.reverse();
-
-            });
-
-
-            //apiService.Search($routeParams.word).then(function (res) {
-            //    $scope.searchTime = res.data.searchTime;
-            //    $scope.files = res.data.data.reverse();
-            //    $scope.data_ready = true;
+            //myService.async().then(function(d) {
+            //    $scope.searchTime = d.data.searchTime;
+            //    $scope.files = d.data.data.reverse();
+            //
             //});
+
+            //console.log('Promise is now resolved: ', myService.doStuff());
+            //
+            //$scope.searchTime = myService.doStuff().searchTime;
+            //$scope.files = myService.doStuff().data.reverse();
+            //$scope.data_ready = true;
+            //
+
+            apiService.Search($routeParams.word).then(function (res) {
+                $scope.searchTime = res.data.searchTime;
+                console.log('Promise is now resolved: ', $scope.searchTime);
+                $scope.files = res.data.data.reverse();
+                $scope.data_ready = true;
+            });
+        }
+
+        $scope.loadResultContent = function (idx) {
+            //alert(b);
+            if (!$scope.files[idx].open) {
+                $scope.files[idx].limit = 12;
+                $scope.files[idx].rrr=$scope.files[idx].positions;
+                $scope.files[idx].open = true;
+            }
+        }
+        $scope.loadMoreResult = function (idx) {
+                $scope.files[idx].limit += 12;
         }
     }
-
-
 );
 
 //
